b5bf6870f3a81c2ee7c33ce6d900f7b86428d3f8
Hariprasad Jayaprakash <hariprasad.becse@gmail.com>
2021-12-11 16:50:25 +0530

    Added MEF 1 example

---
+++ ManagedExtensibilityFramework/Add.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedExtensibilityFramework
+{
+    [Export(typeof(IOperation))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [ExportMetadata("Symbol",'+')]
+    class Add : IOperation, IPartImportsSatisfiedNotification
+    {
+        public Add()
+        {
+
+        }
+        public void OnImportsSatisfied()
+        {
+            Console.WriteLine("Imports satisfied: Add");
+        }
+
+        public int Operate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ManagedExtensibilityFramework/MyAnotherCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedExtensibilityFramework
+{
+    [Export("MyAnotherCalculator", typeof(ICalculator))]
+    public class MyAnotherCalculator : ICalculator, IPartImportsSatisfiedNotification
+    {
+        [ImportMany]
+        IEnumerable<Lazy<IOperation, IOperationData>> operations;
+
+
+        public string Calculate(string input)
+        {
+            int left;
+            int right;
+            char operation;
+            // Finds the operator.
+            int fn = FindFirstNonDigit(input);
+            if (fn < 0) return "Could not parse command.";
+
+            try
+            {
+                // Separate out the operands.
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch
+            {
+                return "Could not parse command.";
+            }
+
+            operation = input[fn];
+
+            foreach (Lazy<IOperation, IOperationData> i in operations)
+            {
+                if (i.Metadata.Symbol.Equals(operation))
+                {
+                    return i.Value.Operate(left, right).ToString();
+                }
+            }
+            return "Operation Not Found!";
+        }
+
+        private int FindFirstNonDigit(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsDigit(s[i])) return i;
+            }
+            return -1;
+        }
+
+        public void OnImportsSatisfied()
+        {
+            Console.WriteLine("Inports Satisfied: MyAnotherCalculator");
+        }
+    }
+}
